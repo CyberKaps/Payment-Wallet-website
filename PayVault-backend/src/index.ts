22ded0da5bb84c95f34095e50ae94c2b6b444843
Kalpesh Borde <bordekalpesh@gmail.com>
2025-03-31 00:41:45 +0530

    added basic skeleton in index.js

--- PayVault-backend/src/index.ts
+++ PayVault-backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello")
+
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT  = process.env.PORT || 3000;
+
+app.use(express.json()); //middleware to parse JSON bodies
+
+
+app.get("/", (req: Request, res: Response) => {
+
+    res.json({
+        message: "payment wallet app"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
