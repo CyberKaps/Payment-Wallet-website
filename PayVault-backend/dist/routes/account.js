f19d640356c7ea9dc75f80da0b3e14b4385cb10f
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-14 00:47:18 +0530

    added transfer route for user to transfer money to another account

--- PayVault-backend/dist/routes/account.js
+++ PayVault-backend/dist/routes/account.js
@@ -8,11 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.accountRouter = void 0;
 const express_1 = require("express");
 const middleware_1 = require("../middleware");
 const db_1 = require("../db");
+const mongoose_1 = __importDefault(require("mongoose"));
 exports.accountRouter = (0, express_1.Router)();
 exports.accountRouter.get("/balance", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const account = yield db_1.Account.findOne({
@@ -20,5 +24,33 @@ exports.accountRouter.get("/balance", middleware_1.authMiddleware, (req, res) =>
     });
     res.json({
         balance: account === null || account === void 0 ? void 0 : account.balance
+    });
+}));
+exports.accountRouter.post("/transfer", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const session = mongoose_1.default.startSession();
+    (yield session).startTransaction();
+    const { amount, to } = req.body;
+    // Fetch the accounts within the transaction
+    const account = yield db_1.Account.findOne({ userId: req.userId }).session(yield session);
+    if (!account || account.balance < amount) {
+        (yield session).abortTransaction();
+        return res.status(400).json({
+            message: "insufficient balance"
+        });
+    }
+    const toAccount = yield db_1.Account.findOne({ userId: to }).session(yield session);
+    if (!toAccount) {
+        (yield session).abortTransaction();
+        return res.status(400).json({
+            message: "invalid account"
+        });
+    }
+    //perform the transfer
+    yield db_1.Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(yield session);
+    yield db_1.Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(yield session);
+    //commit the transaction
+    yield (yield session).commitTransaction();
+    res.json({
+        message: "Transfer successful"
     });
 }));
--- PayVault-backend/src/routes/account.ts
+++ PayVault-backend/src/routes/account.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { authMiddleware } from "../middleware";
 import { Account } from "../db";
+import mongoose from "mongoose";
 
 
 
@@ -14,4 +15,41 @@ accountRouter.get("/balance",authMiddleware,async (req: any, res: any) => {
     res.json({
         balance: account?.balance
     })
+})
+
+
+accountRouter.post("/transfer", authMiddleware, async(req: any, res: any) => {
+    const session = mongoose.startSession();
+
+    (await session).startTransaction();
+    const { amount, to } = req.body;
+
+    // Fetch the accounts within the transaction
+    const account = await Account.findOne({ userId: req.userId}).session(await session);
+
+    if(!account || account.balance < amount){
+        (await session).abortTransaction();
+        return res.status(400).json({
+            message: "insufficient balance"
+        })
+    }
+
+    const toAccount = await Account.findOne({ userId: to}).session(await session);
+
+    if(!toAccount){
+        (await session).abortTransaction();
+        return res.status(400).json({
+            message: "invalid account"
+        })
+    }
+
+    //perform the transfer
+    await Account.updateOne({ userId: req.userId}, { $inc: { balance: -amount }}).session(await session);
+    await Account.updateOne({ userId: to}, { $inc: { balance: amount }}).session(await session);
+
+    //commit the transaction
+    await (await session).commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    });
 })
