5c16b55237bd49ddb5edd257a17dec1d2ea31cf9
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-14 12:01:53 +0530

    created signin page

--- PayVault-frontend/src/App.tsx
+++ PayVault-frontend/src/App.tsx
@@ -1,9 +1,7 @@
 // import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import {SubHeading} from "./components/SubHeading"
-import { InputBox } from './components/InputBox'
-import { Button } from './components/Button'
-import { BottomWarning } from './components/BottomWarning'
+import { Signin } from './pages/Signin'
+
 import { Signup } from './pages/Signup'
 
 function App() {
@@ -19,11 +17,9 @@ function App() {
           <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter> */}
-      <SubHeading label={"Enter your information to create an account"}/>
-      <InputBox label={"First Name"} placeholder={"Kalpesh"} />
-      <Button onClick={() => {}} label={"Sign up"} />
-      <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to="/signin" />
+  
       <Signup />
+      <Signin />
     </>
   )
 }
