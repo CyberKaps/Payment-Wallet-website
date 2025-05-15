fb0bb25063400e0b81aee4817ab3bf5a749461df
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-15 23:36:33 +0530

    created Users function with useState hook

---
+++ PayVault-frontend/src/components/User.tsx
@@ -0,0 +1,13 @@
+
+
+import { useState } from "react";
+import { Button } from "./Button";
+
+
+
+export const Users = () => { 
+    const[users, setUsers] = useState([{
+        firstName: "Kalpesh",
+        lastName: "Borde"
+    }])
+}
