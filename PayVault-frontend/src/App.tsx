9cde9097ea2b84c21d5bee9ef5e847ba57343ad8
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-15 23:54:23 +0530

    added dashboard in routing

--- PayVault-frontend/src/App.tsx
+++ PayVault-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
+import { Dashboard } from './pages/Dashboard'
 
 function App() {
 
@@ -12,8 +13,8 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/send" element={<SendMoney />} /> */}
         </Routes>
       </BrowserRouter>
   
